45f5f1e88bba23b8715ef508f13c5532a0ed773c
AyHa1810 <63386848+AyHa1810@users.noreply.github.com>
2022-02-27 23:30:54 +0530

    Update and rename fileList.php to fileList.js

---
+++ fileList.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+
+const getFileInfoFromFolder = (route) => {
+  const files = fs.readdirSync(route, 'utf8');
+  const response = [];
+  for (let file of files) {
+    const extension = path.extname(file);
+    const fileSizeInBytes = fs.statSync(file).size;
+    const modifiedTime = fs.statSync(file).mtime;
+    response.push({ name: file, extension, fileSizeInBytes, modifiedTime });
+  }
+  return response;
+}
+
+const { name, extenstion, fileSizeInBytes, modifiedTime } = getFileInfoFromFolder(".")
