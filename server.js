e696b3f23a7c1d3f115eeade208a19cd73bde1f5
AyHa1810 <ayanhalder206@gmail.com>
2023-03-10 13:40:32 +0530

    experimenting with github pages

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js : launches the website as a server
+// required dependencies:
+const express = require('express');
+const morlog = require('morgan');
+//const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+//const router = express.Router();
+
+// Set variables
+var PORT = process.env.PORT || 8080;
+var dir = path.join(__dirname, 'src');
+app.set('port', PORT)
+
+// Make express use the given stuff
+app.use(morlog('tiny'));
+app.use(bodyParser.json());
+app.use(express.static(dir));
+
+app.listen(PORT, () => {
+    console.log(`Express Server started on Port ${app.get('port')}`);
+});
